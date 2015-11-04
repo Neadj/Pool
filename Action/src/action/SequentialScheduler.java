49fbde133435f72c36e5ccc7b55de66460e7575d
Voyager2718 <voyager2718@gmail.com>
2015-11-04 16:55:29 +0100

    WTF

---
+++ Action/src/action/SequentialScheduler.java
@@ -0,0 +1,15 @@
+package action;
+
+public class SequentialScheduler extends Scheduler{
+
+	public void reallyDoStep() throws ActionFinishedException, ActionNotInitializedException {
+		if (this.actions.isEmpty()){
+			throw new ActionNotInitializedException();
+		}
+		Action a = this.actions.get(0);
+		a.doStep();
+		if (a.isFinished()){
+			this.actions.remove(0);
+		}
+	}
+}
---
+++ Action/src/resources/BasketPool.java
@@ -0,0 +1,13 @@
+package resources;
+
+public class BasketPool extends ResourcePool<Basket> {
+
+    @Override
+    public Basket createResource() {
+        return new Basket();
+    }
+
+    public BasketPool(int nbResource) {
+        super(nbResource);
+    }
+}
